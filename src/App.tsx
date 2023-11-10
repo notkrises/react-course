cec06fe61aa7fe1a52f0407eac9f76ff4a94fa0b
Krishna Nagar <notkrises@gmail.com>
2023-11-10 13:16:06 -0500

    Version 1

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Message from './Message'
+
+function App()
+{
+  // Always close your react components, in this case using "<Message></Message>" or using the self-closing syntax, "<Message /></"
+  return <div><Message /></div>;
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+// PascalCasing - Capitalize the first letter of every word
+function Message()
+{
+    // JSX: JavaScript XML
+    const name = "Krishna"; 
+    return <h1>Hello {name}</h1>;
+}
+
+export default Message;
