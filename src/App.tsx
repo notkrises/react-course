62786ded5a9a21e13777f36ceef63355cbeae3c9
Krishna Nagar <notkrises@gmail.com>
2023-11-26 13:25:07 -0500

    Creating CSS Modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
+import "./App.css";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
-
+  const items = ["New York", "Los Angeles", "San Francisco"];
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
-      )}
-      <Button color="danger" onClick={() => setAlertVisibility(true)}>
-        My Button
-      </Button>
+      <ListGroup
+        heading="Miami"
+        items={items}
+        onSelectItem={() => console.log("Clicked!")}
+      />
     </div>
   );
 }
--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from "./ListGroup.module.css";
 
 interface Props {
   items: string[];
@@ -13,7 +14,7 @@ const ListGroup = ({ items, heading, onSelectItem }: Props) => {
     <>
       <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
-      <ul className="list-group">
+      <ul className={[styles.listGroup, styles.container].join(" ")}>
         {items.map((item, index) => (
           <li
             className={
