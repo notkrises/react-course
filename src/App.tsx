da2b67d90435a18ce41504e00a54a9cad877b11c
Krishna Nagar <notkrises@gmail.com>
2023-11-26 19:24:22 -0500

    Building A Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
-        consequatur, magnam aut corrupti asperiores eaque similique at quod
-        suscipit nam tempore accusantium! Provident eligendi eveniet optio
-        laudantium quisquam expedita dolor pariatur quae fugiat nobis quod,
-        repellendus voluptatem earum, exercitationem placeat laboriosam
-        architecto. Voluptatem doloribus maxime quam quod, dolor id modi quasi
-        suscipit est omnis velit, necessitatibus ratione. Voluptatum doloribus
-        exercitationem, tempore, sequi quos blanditiis veritatis eos similique
-        officiis ad impedit omnis fuga voluptates deleniti vel! Blanditiis
-        provident veniam nobis, obcaecati ex debitis reprehenderit accusamus
-        enim ratione fuga fugit dolor esse maiores nam fugiat, voluptates,
-        impedit quo? Quas doloremque corrupti cupiditate.
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
