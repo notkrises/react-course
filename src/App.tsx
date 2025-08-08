87de5f6edbd5d1c19946066c8ce4d14b66ba3be0
Krishna Nagar <notkrises@gmail.com>
2025-08-08 16:39:31 -0400

    Creating a List Group Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import Message from "./Message"
+import ListGroup from "./components/ListGroup";
 
 function App() {
   // Two ways of closing: "<Message />" or "<Message></Message>"
-  return <div><Message /></div>
+  return <div><ListGroup /></div>
 }
 
 export default App;
