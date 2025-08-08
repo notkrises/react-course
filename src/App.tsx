1c3f9db9b27f29d0d04c572a8f8bf401aa95a16d
Krishna Nagar <notkrises@gmail.com>
2025-08-08 16:27:07 -0400

    Adding Bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Message from "./Message"
+
+function App() {
+  // Two ways of closing: "<Message />" or "<Message></Message>"
+  return <div><Message /></div>
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,14 @@
+
+// PascalCasing: Capitalizing the first letter of every word: 
+// convention used with react
+function Message() {
+    // JSX: JavaScript XML
+    // 1. This code will get compiled down to JavaScript
+
+    // 2. JSX allows us to easily create dymanic content such as:
+    const name = "Krishna";
+    // return <h1>Hello {name} </h1>;
+    return <h1>Hello {name}</h1>;
+}
+
+export default Message;
