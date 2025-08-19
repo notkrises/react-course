038a9adc06692ce060ab90f03d61509d0859fb3b
Krishna Nagar <notkrises@gmail.com>
2025-08-19 14:23:10 -0400

    Passing Data via Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
   // Two ways of closing: "<Message />" or "<Message></Message>"
-  return <div><ListGroup /></div>
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" />
+    </div>
+  );
 }
 
 export default App;
