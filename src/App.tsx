c625d694b6f1166d21109228afe451f19615d278
Krishna Nagar <notkrises@gmail.com>
2025-08-21 14:55:15 -0400

    Passing Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
-  // Two ways of closing: "<Message />" or "<Message></Message>"
   return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+    <div className="alert alert-danger">
+      <Alert>
+        {" "}
+        Hello <span>Pog</span>{" "}
+      </Alert>
     </div>
   );
 }
