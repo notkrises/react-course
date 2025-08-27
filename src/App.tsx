7d1d16d3151211ad00d2b0b91bb6efeaf906a593
Krishna Nagar <notkrises@gmail.com>
2025-08-27 18:54:08 -0400

    Integrating with React Hook Form and Zod

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-
-export const categories = ["Groceries", "Utilties", "Entertainment"];
+import categories from "./expense-tracker/components/categories";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
